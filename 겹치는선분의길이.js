8c03513a3198f9e07008bb2eff76a4645719ffaa
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2022-12-28 17:09:35 +0900

    겹치는선분의길이

---
+++ 겹치는선분의길이.js
@@ -0,0 +1,17 @@
+function solution(lines) {
+
+    const array = Array(200).fill(0);
+    
+    lines.forEach(([a, b]) => {
+        
+        if (a > b) {
+            [a, b] = [b, a];
+        }
+        
+        for (let i = a; i < b; i++) {
+            array[i + 100] += 1;
+        }
+    });
+
+    return array.filter(e => e > 1).length;
+}
