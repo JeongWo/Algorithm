360633b22be0a9129f5320305cb2de968803ef95
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-06-02 23:24:24 +0900

    둘만의암호

---
+++ 둘만의암호.js
@@ -0,0 +1,18 @@
+function solution(s, skip, index) {
+    let last = 'z'.charCodeAt();
+    skip = skip.split('').map((v) => v.charCodeAt());
+    return s.split('')
+        .map((v) => v.charCodeAt())
+        .map((v) => {
+          let newIndex = index;
+          for (let a = 0; a < newIndex; a++) {
+            if (v === last) {
+              v -= 26;
+            }
+            v++;
+            skip.includes(v) && newIndex++;
+          }
+          return String.fromCharCode(v);
+        })
+        .join('');
+  };
