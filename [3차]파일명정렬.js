ea8e0b173ae7458cace9db83bda4dc8de6189429
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-08-02 23:38:01 +0900

    [3차] 파일명 정렬

---
+++ [3차]파일명정렬.js
@@ -0,0 +1,14 @@
+function solution(files) {
+    return files.sort((a, b) => {
+      let aHead = a.match(/^\D+/)[0].toLowerCase();
+      let bHead = b.match(/^\D+/)[0].toLowerCase();
+  
+      if(aHead < bHead) return -1;
+      if(aHead > bHead) return 1;
+  
+      let aNum = a.match(/\d+/)[0].replace(/^0+/, '');
+      let bNum = b.match(/\d+/)[0].replace(/^0+/, '');
+  
+      return aNum - bNum;
+    });
+  }
