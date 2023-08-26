abcf14d89f50a7df70054c7884e2d98627a6ea16
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-08-26 23:05:12 +0900

    디펜스게임

---
+++ 디펜스게임.js
@@ -0,0 +1,25 @@
+function solution(n, k, enemy) {
+    let answer = 0;
+    let start = 0; 
+    let end = enemy.length;
+    
+    while(start<=end){
+        let mid = Math.floor((start+end)/2);
+        let arr = enemy.slice(0, mid).sort((a, b)=> b-a);
+        
+        let flag = true;
+        let temp = 0 ;
+        for(let i=k ; i<arr.length; i++){
+            temp+=arr[i];
+            if(temp>n) flag = false;
+        }
+        
+        if(flag) {
+            answer = mid;
+            start = mid+1; 
+        }
+        else end = mid-1;
+    }
+    
+    return answer;
+}
